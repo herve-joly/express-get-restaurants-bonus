dea3909afd80acc049a73456ba89848a95c27da6
= <=>
2023-02-22 20:26:58 +0000

    not working

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const port = 3000;
-const {Restaurant, Menu, Item} = require("./models/index")
-const {sequelize} = require("./db")
+const { Restaurant, Menu, Item } = require("./models/index");
+const { sequelize } = require("./db");
 
-//TODO: 
+//TODO:
+app.use(express.json);
+app.get("/restaurants", async (request, response) => {
+  const respond = await Restaurant.findAll({
+    include: [{ model: Menu, include: [Item] }],
+  });
+
+  response.send(respond);
+});
 
 app.listen(port, () => {
-    sequelize.sync()
-    console.log("App listening on port " + port)
-})
+  sequelize.sync();
+  console.log("App listening on port " + port);
+});
